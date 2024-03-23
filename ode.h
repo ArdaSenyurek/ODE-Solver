86717e039bdb099ea77c36e943d9ef8dbd2defa3
Arda Senyurek <ardasenyurekgit@gmail.com>
2024-03-23 05:42:46 +0300

    first commit

--- ode.h
+++ ode.h
@@ -1,5 +1,5 @@
-#include "tensor.h"
-#include <math>
+#include "../modules/tensor.h"
+#include "../modules/uint.h"
 
 template <class qnt>
 class ode
@@ -8,18 +8,19 @@ class ode
 		uint rank_;
 		float endTime_;
 		float dT_;
-		qnt* inits_;
-		qnt* current_;
-		qnt  (*rule_)(tensor<qnt>);
-		bus* wire_;
+		tensor<qnt>* inits_;
+		tensor<qnt>* current_;
+		qnt  (*rule_)(tensor<qnt>*);
+		//bus* wire_;
 
 	public:
-		ode(uint rank,
+		ode(int rank,
 		    float end,
 		    float dt,
-		    qnt* inVal,
-		    tensor<qnt> (*funcPtr)(tensor<qnt>),
-		    bus* wire) 
+		    tensor<qnt>* inVal,
+		    qnt (*funcPtr)(tensor<qnt>*)
+		    //bus* wire
+		    ) 
 
 		    :
 		    rank_(rank),
@@ -27,18 +28,20 @@ class ode
 		    dT_(dt),
 		    inits_(inVal),
 		    current_(inits_),
-		    wire_(wire)
+		    rule_(funcPtr)
+		    //wire_(wire)
 		{}
 
 		virtual void solveStep() = 0;
 		
-		virtual tensor<qnt> dot()
+		virtual tensor<qnt> dot(tensor<qnt>& ModifiedCurrent)
 		{
 			tensor<qnt> res(rank_ + 2, 1);
-			res[0]  = rule_(current);
+			res(0,0)  = rule_(&ModifiedCurrent);
 
 			for(int x = 1; x < rank_ + 2; x++)
-				res[x] = current_[x - 1];
+				// TODO: cant modify the tensor externally. Tweak the operator.
+				res(x, 0) = ModifiedCurrent[x - 1];
 			return res;
 		}
 		
@@ -47,30 +50,50 @@ class ode
 			for(uint i = 0; i < dT_; i++)
 			{
 				solveStep();
-				wire_ -> update(current_);
+				//wire_ -> update(current_);
 			}
 		}
 };
-
-template <class qnt>
-class rungeKutta4 : public ode
+template<class qnt>
+class rungeKutta4 : ode<qnt>
 {
 	void solveStep() override
 	{
-		tensor<qnt> a = dot(current_);
-		tensor<qnt> b = dot(current_ + dT_ / 2 * a);
-		tensor<qnt> c = dot(current_ + dT_ / 2 * b);
-		tensor<qnt> d = dot(current_ + dT_ * c);
-		
-		current_ += h/6 * (a + 2 * b + 2 * c + d);
+		tensor<qnt> a = this -> dot(*(this -> current_));
+		tensor<qnt> b = this -> dot(*(this -> current_) +  1 /(this -> dT_ / 2) / a);
+		tensor<qnt> c = this -> dot(*(this -> current_) +  1 /(this -> dT_ / 2) / b);
+		tensor<qnt> d = this -> dot(*(this -> current_) +  1 /this ->dT_ / c);
+		                                                            
+		this -> current_ += this -> dT_ /6 * (a + 2 * b + 2 * c + d);
 	}
 };
 
-class euler : public ode
+template<class qnt>
+class euler : ode<qnt>
 {
+	public:
+		euler(int rank,
+		    float end,
+		    float dt,
+		    tensor<qnt>* inVal,
+		    qnt(*funcPtr)(tensor<qnt>*)
+		    //bus* wire
+		    ) 
+
+		    :
+
+		    ode<qnt>(rank,
+			     end,
+			     dt,
+			     inVal,
+			     funcPtr
+			     //bus* wire
+		    ) 
+		    {}
 
 	void solveStep() override
 	{
-		current_ += dT_ * dot(current_);
+		tensor<qnt>& currentRef = *(this -> current_);
+		currentRef =  currentRef + this -> dot(*(this -> current_)) / (1/(this -> dT_));
 	}
 };
