642b606438e48a5b2539be01654692dc61959e9c
Arda Senyurek <ardasenyurekgit@gmail.com>
2024-04-06 10:08:53 +0100

    tried higher order 1 equation

--- main.cpp
+++ main.cpp
@@ -6,15 +6,16 @@ tensor<double> dotFuncs(const tensor<double>);
 
 
 int main(){
-	tensor<double> in(2,1,0);
-	in.set(1,1, 5);
+	tensor<double> in(3,1,0);
+	in.set(1,1, 13);
+	in.set(2,1, 7);
 	in.print();
 	in.printLinear();
 	in[1];
 	dotFuncs(in);
 	tensor<double> (*dotRulePtr)(tensor<double>);
 	dotRulePtr = &dotFuncs;
-	euler<double> solver = euler<double>(in, 1.5, 3, dotRulePtr);
+	euler<double> solver = euler<double>(in, 0.25, 0.25, dotRulePtr);
 	ode<double>* solverPtr = &solver;
 
 	solverPtr -> solve();
@@ -30,20 +31,9 @@ tensor<double> dotFuncs(const tensor<double> state)
 	tensor <double> dotTensor(state, -999);
        dotTensor = {
 	
-		2 * t,
-		phi,
-		1
+	       5.5 * exp(-t) - 1.5 * phi - 2.5 * y,
+	       phi,
+	       1
 	};
        return dotTensor;
-/*
-	double y 	= state[1].get();
-	double t 	= state[2].get();
-	
-	tensor <double> dotTensor(state, -999);
-       dotTensor = {
-		3 * exp(-t) - 0.4 * y,
-		1
-	};
-       return dotTensor;
-	*/
 }
